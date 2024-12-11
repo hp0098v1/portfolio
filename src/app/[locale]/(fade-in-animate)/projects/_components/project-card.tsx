fa56f0282d314eebfbf4751a10e0a301ffbd1a19
hp0098v1 <hp0098v1@gmail.com>
2024-12-11 18:06:16 +0330

    add real data for projects

--- src/app/[locale]/(fade-in-animate)/projects/_components/project-card.tsx
+++ src/app/[locale]/(fade-in-animate)/projects/_components/project-card.tsx
@@ -1,17 +1,48 @@
 import { ProjectLinks } from '@/app/[locale]/(fade-in-animate)/projects/_components/project-links';
 import { Project } from '@/app/[locale]/(fade-in-animate)/projects/page';
 import { Badge } from '@/components/ui/badge';
-import { Card, CardContent } from '@/components/ui/card';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
 
-type Props = Pick<Project, 'title' | 'description' | 'tech' | 'slug' | 'links'>;
+type Props = Pick<
+  Project,
+  | 'title'
+  | 'description'
+  | 'tech'
+  | 'slug'
+  | 'links'
+  | 'client'
+  | 'privateSource'
+  | 'privateDemo'
+>;
 
-export function ProjectCard({ title, description, tech, slug, links }: Props) {
+export function ProjectCard({
+  title,
+  description,
+  tech,
+  slug,
+  links,
+  client,
+  privateSource,
+  privateDemo,
+}: Props) {
   return (
     <Card>
+      <CardHeader>
+        <CardTitle className='flex items-start justify-between gap-2'>
+          <h3 className='text-2xl font-bold'>{title}</h3>
+          <Badge>{client}</Badge>
+        </CardTitle>
+        <CardDescription className='text-muted-foreground'>
+          {description}
+        </CardDescription>
+      </CardHeader>
       <CardContent className='p-6'>
-        <h2 className='mb-4 text-2xl font-bold'>{title}</h2>
-        <p className='mb-6 text-muted-foreground'>{description}</p>
-
         <div className='mb-6 flex flex-wrap gap-2'>
           {tech.map((tech, index) => (
             <Badge key={index} variant='outline'>
@@ -20,7 +51,12 @@ export function ProjectCard({ title, description, tech, slug, links }: Props) {
           ))}
         </div>
 
-        <ProjectLinks links={links} slug={slug} />
+        <ProjectLinks
+          links={links}
+          showGithub={!privateSource}
+          showLiveDemo={privateDemo}
+          slug={slug}
+        />
       </CardContent>
     </Card>
   );
